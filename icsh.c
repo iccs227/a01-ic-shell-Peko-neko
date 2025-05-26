f56cdc0854b2842d47bc8b5927e026d368253e96
Peko-neko <66202899+Peko-neko@users.noreply.github.com>
2025-05-27 02:29:32 +0700

    handle_command added

--- icsh.c
+++ icsh.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 #define MAX_CMD_BUFFER 255
 
-int main() {
+void handle_command(char* buffer, char* last_command, int* exit_code) {
+    // Remove newline if present
+    buffer[strcspn(buffer, "\n")] = 0;
+
+    // Ignore empty line
+    if (strlen(buffer) == 0) return;
+
+    // Handle '!!'
+    if (strcmp(buffer, "!!") == 0) {
+        if (strlen(last_command) == 0) {
+            return;
+        }
+        printf("%s\n", last_command);
+        strcpy(buffer, last_command);
+    }
+    else {
+        strcpy(last_command, buffer);
+    }
+
+    // Tokenize and process
+    char* cmd = strtok(buffer, " ");
+    if (!cmd) return;
+
+    if (strcmp(cmd, "echo") == 0) {
+        char* arg = strtok(NULL, "");
+        if (arg) printf("%s\n", arg);
+    }
+    else if (strcmp(cmd, "exit") == 0) {
+        char* arg = strtok(NULL, " ");
+        if (arg) {
+            int code = atoi(arg);
+            *exit_code = code & 0xFF;
+        }
+        printf("bye\n");
+        exit(*exit_code);
+    }
+    else {
+        printf("bad command\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
     char buffer[MAX_CMD_BUFFER];
-    char last_command[MAX_CMD_BUFFER] = "";  // stores last command
+    char last_command[MAX_CMD_BUFFER] = "";
     int exit_code = 0;
 
     FILE* input = stdin;
 
-    // Script mode: open file instead of stdin
     if (argc > 1) {
         input = fopen(argv[1], "r");
         if (!input) {
@@ -25,56 +64,17 @@ int main() {
     }
 
     while (1) {
-        printf("icsh $ ");
-        if (!fgets(buffer, MAX_CMD_BUFFER, stdin)) {
-            // EOF (e.g., Ctrl+D)
-            printf("\nbye\n");
-            break;
+        if (input == stdin) {
+            printf("icsh $ ");
         }
 
-        // Remove trailing newline if present
-        buffer[strcspn(buffer, "\n")] = 0;
-
-        // Handle empty input
-        if (strlen(buffer) == 0) {
-            continue;
-        }
-
-        // Handle '!!' command
-        if (strcmp(buffer, "!!") == 0) {
-            if (strlen(last_command) == 0) {
-                // No previous command
-                continue;
-            }
-            printf("%s\n", last_command);
-            strcpy(buffer, last_command);  // replace current buffer
-        }
-        else {
-            strcpy(last_command, buffer);  // update last_command
-        }
-
-        // Tokenize the input to check the command
-        char* cmd = strtok(buffer, " ");
-
-        if (strcmp(cmd, "echo") == 0) {
-            char* arg = strtok(NULL, "");  // Get rest of the line
-            if (arg) {
-                printf("%s\n", arg);
-            }
-            continue;
-        }
-        else if (strcmp(cmd, "exit") == 0) {
-            char* arg = strtok(NULL, " ");
-            if (arg) {
-                int code = atoi(arg);
-                exit_code = code & 0xFF;
-            }
+        if (!fgets(buffer, MAX_CMD_BUFFER, input)) {
+            if (input != stdin) fclose(input);
             printf("bye\n");
             break;
         }
-        else {
-            printf("bad command\n");
-        }
+
+        handle_command(buffer, last_command, &exit_code);
     }
 
     return exit_code;
